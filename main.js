37e36152870e86928a21f26ec3e0d1d801ac2eb4
jonas <odin@haggei.de>
2020-05-16 18:00:03 +0200

    Add html, js, css pages and very basic calculation

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let currentValue = 0;
+
+function add (val) {
+    return currentValue += val;
+}
+
+function subtract (val) {
+    return currentValue -= val;
+}
+
+function multiply (val) {
+    return currentValue *= val;
+}
+
+function divide (val) {
+    return currentValue /= val;
+}
+
